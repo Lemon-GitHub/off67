2bf9ed278353813379074deb745aa03ee3ad48f5
liu <1373788543@qq.com>
2020-03-02 21:31:53 +0800

    从尾到头打印单链表

---
+++ src/Q3.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+import java.util.ArrayList;
+
+public class Q3 {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        for(ListNode cur = listNode; cur != null; cur = cur.next) {
+            stack.push(cur.val);
+        }
+        while(!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        return result;
+    }
+}
